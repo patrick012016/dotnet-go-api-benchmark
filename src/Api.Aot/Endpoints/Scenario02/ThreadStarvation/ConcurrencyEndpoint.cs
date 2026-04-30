d1344796b76f83e5d4366062c4c14045ebc50ee9
Patryk Górniak <patryk012016@gmail.com>
2026-04-30 22:22:12 +0200

    refactor: extracted the lambda in AOT ConcurrencyEndpoint to an internal static method

--- src/Api.Aot/Endpoints/Scenario02/ThreadStarvation/ConcurrencyEndpoint.cs
+++ src/Api.Aot/Endpoints/Scenario02/ThreadStarvation/ConcurrencyEndpoint.cs
@@ -7,24 +7,25 @@ public static class ConcurrencyEndpoint
 {
     public static void MapConcurrencyTasks(this RouteGroupBuilder group)
     {
-        group.MapGet("/concurrent-tasks",
-                async Task<Results<Ok<StarvationResponse>, ProblemHttpResult>> (CancellationToken ct) =>
-                {
-                    var sw = Stopwatch.StartNew();
-
-                    var tasks = new Task[5];
-                    for (int i = 0; i < 5; i++)
-                    {
-                        tasks[i] = SimulateExternalCallAsync(ct);
-                    }
-
-                    await Task.WhenAll(tasks);
-                    sw.Stop();
-
-                    return TypedResults.Ok(new StarvationResponse("Success", tasks.Length, sw.ElapsedMilliseconds));
-                })
+        group.MapGet("/concurrent-tasks", HandleAsync)
             .WithName("StarvationConcurrentTasks")
             .Produces<StarvationResponse>();
+    }
+
+    internal static async Task<Results<Ok<StarvationResponse>, ProblemHttpResult>> HandleAsync(CancellationToken ct)
+    {
+        var sw = Stopwatch.StartNew();
+
+        var tasks = new Task[5];
+        for (int i = 0; i < 5; i++)
+        {
+            tasks[i] = SimulateExternalCallAsync(ct);
+        }
+
+        await Task.WhenAll(tasks);
+        sw.Stop();
+
+        return TypedResults.Ok(new StarvationResponse("Success", tasks.Length, sw.ElapsedMilliseconds));
     }
 
 
